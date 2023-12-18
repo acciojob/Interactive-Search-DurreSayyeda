3c45c4e26ec23ab41abc4227652baf498d2420a5
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-19 00:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let box=document.querySelector("div");
+let input=document.querySelector(".input");
+let button=document.querySelector(".btn");
+
+button.addEventListener("click",Onclick);
+function Onclick(){
+	box.classList.add("active");
+	// JavaScript code
+input.focus();
+
+}
